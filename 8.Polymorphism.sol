1b9908948db2eb0a0310551916d50794538a3be4
natulos <natulos@gmail.com>
2021-08-10 08:11:26 -0500

    Update 8.Polymorphism.sol

--- 8.Polymorphism.sol
+++ 8.Polymorphism.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-//Functions with the same name, but diferentes parameters
+//Functions with the same name, but different parameters
 
 contract FunctionPolymorphism {
     
