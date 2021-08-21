c829f24ed0d90209ece78e3d2e25b3f8b2a0752c
natulos <natulos@gmail.com>
2021-08-20 19:12:08 -0500

    commit to main

---
+++ 19.EventLogs.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SimpleAction{
+
+    //Event
+    event EventTrigger(address bidder, uint amount); 
+
+    // Trigger event
+    function bid() public payable{
+        emit EventTrigger(msg.sender, msg.value);
+    }
+}
