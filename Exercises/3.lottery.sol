cacee47733c9c86050b1d2cf05e6e989a4934e5a
natulos <natulos@gmail.com>
2021-09-05 11:36:09 -0500

    update

---
+++ Exercises/3.lottery.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0; 
+
+contract Lottery{
+    address public manager;
+    address payable[] public players;
+    uint public index;
+
+    constructor() payable{
+        manager = msg.sender;
+    }
+
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
+
+    function enter() public payable{
+        require(msg.value == .2 ether);
+        players.push(payable(msg.sender));
+    }
+
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public restricted{
+        index = random() % players.length;    
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    function getPlayers() public view returns(address payable[] memory){
+        return players;
+    }
+}
