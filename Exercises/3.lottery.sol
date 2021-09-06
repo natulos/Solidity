7ed248a3f4dc6e67b1d4210eabf1e4218f4b6685
natulos <natulos@gmail.com>
2021-09-05 21:18:47 -0500

    update

--- Exercises/3.lottery.sol
+++ Exercises/3.lottery.sol
@@ -10,24 +10,25 @@ contract Lottery{
         manager = msg.sender;
     }
 
+    //function modifiers
     modifier restricted(){
         require(msg.sender == manager);
         _;
     }
 
     function enter() public payable{
-        require(msg.value == .2 ether);
+        require(msg.value == 2 ether);
         players.push(payable(msg.sender));
     }
 
     function random() public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players))); //generate a random number
     }
 
     function pickWinner() public restricted{
         index = random() % players.length;    
         players[index].transfer(address(this).balance);
-        players = new address payable[](0);
+        players = new address payable[](0); //Clear players array
     }
 
     function getPlayers() public view returns(address payable[] memory){
